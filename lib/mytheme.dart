a924b52c3ffb89dc582753480d68fe638cfa5ed0
FlutLab.io <>
2021-08-23 15:22:42 +0000

    Snapshot 'new blog pattern is created'

--- lib/mytheme.dart
+++ lib/mytheme.dart
@@ -1,83 +1,44 @@
-//import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
+
 
 final darkTheme = ThemeData(
-  primarySwatch: Colors.blue,
+  primarySwatch: Colors.grey,
   primaryColor: Colors.black,
   brightness: Brightness.dark,
-  canvasColor: const Color(0xFF515556),
-  backgroundColor: Colors.grey[700],
+  canvasColor: const Color(0xFF575a5b),
+  backgroundColor: Colors.transparent,
   accentColor: Colors.white,
   accentIconTheme: IconThemeData(color: Colors.black),
-  dividerColor: Colors.white54,
-  //hashColor: Colors.grey[500],
-  textTheme: const TextTheme(
-    headline1: TextStyle(fontSize: 70.0, fontWeight: FontWeight.bold),
-    headline2: TextStyle(fontSize: 28.0),
-    headline3: TextStyle(fontSize: 20, fontFamily: 'Pyidaungsu'),
-    headline5: TextStyle(fontSize: 14.0),
-    headline6: TextStyle(fontSize: 11.0),
-    bodyText2: TextStyle(fontSize: 16.0, fontFamily: 'Pyidaungsu'),
-  ),
+  dividerColor: Colors.black12,
 );
 
 final lightTheme = ThemeData(
   primarySwatch: Colors.grey,
   primaryColor: Colors.blue,
   brightness: Brightness.light,
-  backgroundColor: Colors.grey[300],
+  
+  backgroundColor: const Color(0xFFE5E5E5),
   accentColor: Colors.black,
+
   accentIconTheme: IconThemeData(color: Colors.green),
-  dividerColor: Colors.black12,
-  //hashColor: Colors.grey[200],
-  textTheme: const TextTheme(
-    headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-    headline2: TextStyle(fontSize: 28),
-    headline3: TextStyle(fontSize: 20, fontFamily: 'Pyidaungsu'),
-    headline5: TextStyle(fontSize: 14.0),
-    headline6: TextStyle(fontSize: 11.0),
-    bodyText2: TextStyle(fontSize: 16.0, fontFamily: 'Pyidaungsu'),
-  ),
+  dividerColor: Colors.white54,
 );
 
 class ThemeNotifier with ChangeNotifier {
-  bool _isdark = false;
-  get getTheme => _isdark ? darkTheme : lightTheme;
-  get isDark => _isdark;
-  bool isdark() {
-    return _isdark;
-  }
-
-  final String key = "theme";
-  SharedPreferences? _pref;
-  ThemeNotifier() {
-    _loadFromPrefs();
-  }
-  _initPrefs() async {
-    if (_pref == null) _pref = await SharedPreferences.getInstance();
-  }
-
-  _loadFromPrefs() async {
-    await _initPrefs();
-    _isdark = _pref?.getBool(key) ?? true;
-    notifyListeners();
-  }
-
-  _saveToPrefs() async {
-    await _initPrefs();
-    _pref?.setBool(key, _isdark);
-  }
-
+  
+  bool _isdark=false;
+  get getTheme =>_isdark?darkTheme:lightTheme;
+  get isDark =>_isdark;
   setTheme(bool isdark) {
-    _isdark = isdark;
-    _saveToPrefs();
+    _isdark=isdark;
     notifyListeners();
   }
-
-  setAlt() {
-    _isdark = !_isdark;
-    _saveToPrefs();
+  setAlt(){
+    _isdark=!_isdark;
     notifyListeners();
   }
 }
+
+
