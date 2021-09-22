7b03a72e7486e32c6ff337dc503cd9246baac469
FlutLab.io <>
2021-09-22 14:43:51 +0000

    Autosave

--- lib/home/play/flist/flist.dart
+++ lib/home/play/flist/flist.dart
@@ -27,7 +27,7 @@ class flist extends StatelessWidget {
         itemBuilder: (context, index) {
           return Card(
               child: SizedBox(
-                  height: 100,
+                  height: 50,
                   child: ListTile(
                       title: Text(
                         'Pack ${index + 1}',
